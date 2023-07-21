0e33cb4cec5ca574f929872766542235a0c4656e
Tristan <LeoDog896@hotmail.com>
2023-07-21 15:56:36 -0400

    refactor: reorganize, init sveltekit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import wasmPack from 'vite-plugin-wasm-pack';
+
+export default defineConfig({
+	plugins: [
+		wasmPack("./elementary-cellular-automaton"),
+		sveltekit()
+	]
+});
