3fdc00867911a13d1955e5a540eb4b8ec3b9fc01
Tristan <LeoDog896@hotmail.com>
2023-07-21 16:05:06 -0400

    feat: init wasm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import init from "elementary-cellular-automaton"
+	import { onMount } from "svelte";
+
+    onMount(async () => {
+        await init();
+    })
+</script>
